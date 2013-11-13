bc2437c889ef1042d9ea14dd1dd9b710021a8ed1
wouter83 <wouterkok@gmail.com>
2013-11-13 19:01:13 +0100

    Update find.sh

--- find.sh
+++ find.sh
@@ -1,2 +1,6 @@
+#!/bin/sh
+#
+# What does this do here? i have no idea
+#
 cd -P "$( dirname "$1" )" && echo "$( dirname "$1" )" && subliminal -l en -s opensubtitles -s addic7ed -s bierdopje --cache-dir=/home/user/cache .
 
